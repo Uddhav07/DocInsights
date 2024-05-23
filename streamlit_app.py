d698c93a8ce7b3ecad7296b2496918c9868b2420
Uddhav Mittal <93899908+Uddhav07@users.noreply.github.com>
2024-05-23 08:57:14 +0000

    added ollama

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,18 @@
 import streamlit as st
-import pandas as pd
-from io import StringIO
+# import pandas as pd
+# from io import StringIO
 
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_community.llms import Ollama
+
+
+prompt=ChatPromptTemplate.from_messages(
+    [
+        ("system","Help the user understand things better"),
+        ("user","Question:{question}")
+    ]
+)
 
 st.title("DocInsight")
 
@@ -15,18 +26,23 @@ for message in st.session_state.messages:
         st.markdown(message["content"])
 
 
+llm=Ollama(model="phi3")
+output_parser=StrOutputParser()
+chain=prompt|llm|output_parser
+
+
 # React to user input
-if prompt := st.chat_input("What is up?"):
+if prompt := st.chat_input("Yooo wassup?"):
     # Display user message in chat message container
     st.chat_message("user").markdown(prompt)
     # Add user message to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
 
-    response = f"Echo: {prompt}"
+    response = chain.invoke({"question":prompt})
     # Display assistant response in chat message container
     with st.chat_message("assistant"):
         st.markdown(response)
     # Add assistant response to chat history
     st.session_state.messages.append({"role": "assistant", "content": response})
     
-uploaded_file = st.file_uploader("Choose a file", accept_multiple_files=1)
+# uploaded_file = st.file_uploader("Choose a file", accept_multiple_files=1)
