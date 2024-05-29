61e238cba00cbd53ef898db6b2fe6a86c05936a5
Uddhav Mittal <93899908+Uddhav07@users.noreply.github.com>
2024-05-29 07:42:00 +0000

    vectore_store options added

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,7 @@ embeddings = HuggingFaceEmbeddings()
 # from langchain_community.embeddings import OllamaEmbeddings
 # embeddings = OllamaEmbeddings()
 from langchain_community.vectorstores import FAISS
+from langchain_community.vectorstores import Chroma
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 text_splitter=RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=50)
 from langchain.chains.combine_documents import create_stuff_documents_chain
@@ -34,7 +35,6 @@ I will tip you $1000 if the user finds the answer helpful.
 Question: {input}""")
 #Think step by step before providing a detailed answer using chain of verification (COVE)
 
-
 st.title("DocInsight")
 
 # Initialize chat history
@@ -46,21 +46,39 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
+models = ("qwen:0.5b","tinyllama")
+vstores = ("FAISS","Chroma")
 
-llm=Ollama(model="qwen:0.5b")
-output_parser=StrOutputParser()
-chain=prompt2|llm|output_parser
+if "model_selected" not in st.session_state:
+    st.session_state.model_selected = "qwen:0.5b"
+if "store_selected" not in st.session_state:
+    st.session_state.store_selected = "FAISS"
 
+def chain_maker():
+    model_selected = st.session_state.model_selected
+    global chain,document_chain
+    llm=Ollama(model=model_selected)
+    output_parser=StrOutputParser()
+    chain=prompt2|llm|output_parser
+    document_chain=create_stuff_documents_chain(llm,prompt)
 
-document_chain=create_stuff_documents_chain(llm,prompt)
-
-with st.sidebar:
-    uploaded_files = st.file_uploader("Please upload your files", accept_multiple_files=True, type=None)
+chain_maker()
 prompt = st.chat_input("Yooo wassup?")
 
+def vstore(document_chunks):
+    store_selected = st.session_state.store_selected
+    if store_selected == "FAISS":
+        vectorstore = FAISS.from_documents(document_chunks, embeddings)
+    if store_selected == "Chroma":
+        vectorstore = Chroma.from_documents(document_chunks, embeddings)
+    # Store the processed data in session state for reuse
+    st.session_state.processed_data = {
+        "document_chunks": document_chunks,
+        "vectorstore": vectorstore,
+    }
 
-    
 def uploaded():
+    global document_chunks
     if uploaded_files:
         global vectorstore
         # Print the number of files uploaded or YouTube URL provided to the console
@@ -102,17 +120,11 @@ def uploaded():
         text_splitter = RecursiveCharacterTextSplitter(chunk_size=1500, chunk_overlap=150)
         document_chunks = text_splitter.split_documents(documents)
 
-        vectorstore = FAISS.from_documents(document_chunks, embeddings)
-
-        # Store the processed data in session state for reuse
-        st.session_state.processed_data = {
-            "document_chunks": document_chunks,
-            "vectorstore": vectorstore,
-        }
-            
+        vstore(document_chunks)
+      
 def prompted():        
     if prompt:
-        
+        vectorstore = st.session_state.processed_data["vectorstore"]
         # Display user message in chat message container
         st.chat_message("user").markdown(prompt)
         # Add user message to chat history
@@ -127,7 +139,7 @@ def prompted():
         except:
             response = chain.invoke(prompt)
             flag=1
-        if flag == 0:
+        if flag == 1:
         # Display assistant response in chat message container
             with st.chat_message("assistant"):
                 st.markdown(response)
@@ -135,13 +147,20 @@ def prompted():
             st.session_state.messages.append({"role": "assistant", "content": response})
         else:
             with st.chat_message("assistant"):
-                st.markdown(response["answer"])
+                st.markdown(response)
             # Add assistant response to chat history
-            st.session_state.messages.append({"role": "assistant", "content": response["answer"]})
+            st.session_state.messages.append({"role": "assistant", "content": response})
 
-  
-if (uploaded_files):
-    uploaded()
+with st.sidebar:
+    uploaded_files = st.file_uploader("Please upload your files", accept_multiple_files=True, type=None)
+    st.button("index files", on_click=uploaded)
+    model_selected = st.selectbox("Select Model",models)
+    store_selected = st.selectbox("select store", vstores)
+    if model_selected != st.session_state.model_selected:
+        st.session_state.model_selected = model_selected
+        chain_maker()
+    if store_selected != st.session_state.store_selected:
+        st.session_state.store_selected = store_selected
+        vstore(document_chunks)
 if prompt:
     prompted()
-    uploaded()
