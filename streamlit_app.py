cc9217e20f2e7fcb4a0b062692928e75303d70cc
Uddhav Mittal <93899908+Uddhav07@users.noreply.github.com>
2024-05-29 07:12:51 +0000

    lets see

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,36 +6,38 @@ from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_community.llms import Ollama
-import PyPDF2
 from langchain_community.embeddings import HuggingFaceEmbeddings
 embeddings = HuggingFaceEmbeddings()
 # from langchain_community.embeddings import OllamaEmbeddings
 # embeddings = OllamaEmbeddings()
 from langchain_community.vectorstores import FAISS
+
+# __import__('pysqlite3')
+# import sys
+# sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+from langchain_community.vectorstores import Chroma
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 text_splitter=RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=50)
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains import create_retrieval_chain
 
 prompt = ChatPromptTemplate.from_template("""
-Prefferably Answer the following question based  on the provided context. 
-Only If much reference is not available in the context then start the answer with [NotDoc] and answer on your own
-answer in very short very fast
+IMPORTANT!!! Answer the following question based on only the provided context and nothing else.
+You are a precise, Q&A system that only answers the question based on the given data file. You are not allowed to make up or create content. If you do not know the answer, please just respond I do not know. Keep the answer as concise as possible and return answers as a bullet point if possible.
+if context is not provided or is not useful dont answer at all. 
+answer in very short very fast.
 I will tip you $1000 if the user finds the answer helpful. 
 <context>
 {context}
 </context>
 Question: {input}""")
+st.title("DocInsight")
 
 prompt2 = ChatPromptTemplate.from_template("""
 Prefferably Answer the following question based  on the provided context. 
 answer in very short very fast
 I will tip you $1000 if the user finds the answer helpful. 
 Question: {input}""")
-#Think step by step before providing a detailed answer using chain of verification (COVE)
-
-
-st.title("DocInsight")
 
 # Initialize chat history
 if "messages" not in st.session_state:
@@ -46,22 +48,32 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
+models = ("qwen:0.5b","tinyllama")
+vstores = ("FAISS","Chroma")
 
-llm=Ollama(model="qwen:0.5b")
-output_parser=StrOutputParser()
-chain=prompt2|llm|output_parser
+if "model_selected" not in st.session_state:
+    st.session_state.model_selected = "qwen:0.5b"
+if "store_selected" not in st.session_state:
+    st.session_state.store_selected = "FAISS"
+
+def chainmaker():
+    model_selected = st.session_state.model_selected
+    st.write(model_selected)
+    global chain
+    global document_chain
+    llm=Ollama(model=model_selected)
+    output_parser=StrOutputParser()
+    chain=prompt2|llm|output_parser
+    document_chain=create_stuff_documents_chain(llm,prompt)
 
 
-document_chain=create_stuff_documents_chain(llm,prompt)
-
-with st.sidebar:
-    uploaded_files = st.file_uploader("Please upload your files", accept_multiple_files=True, type=None)
-prompt = st.chat_input("Yooo wassup?")
+chainmaker()
 
 
-    
+
 def uploaded():
     if uploaded_files:
+        store_selected = st.session_state.store_selected
         global vectorstore
         # Print the number of files uploaded or YouTube URL provided to the console
         st.write(f"Number of files uploaded: {len(uploaded_files)}")
@@ -101,24 +113,25 @@ def uploaded():
         # Chunk the data, create embeddings, and save in vectorstore
         text_splitter = RecursiveCharacterTextSplitter(chunk_size=1500, chunk_overlap=150)
         document_chunks = text_splitter.split_documents(documents)
-
+        # if store_selected == "FAISS":
         vectorstore = FAISS.from_documents(document_chunks, embeddings)
-
+        # else:
+        # vectorstore = Chroma.from_documents(document_chunks, embeddings)
         # Store the processed data in session state for reuse
         st.session_state.processed_data = {
             "document_chunks": document_chunks,
             "vectorstore": vectorstore,
         }
-            
+prompt = st.chat_input("Yooo wassup?")
+    
 def prompted():        
     if prompt:
-        
+        global doc_context
         # Display user message in chat message container
         st.chat_message("user").markdown(prompt)
         # Add user message to chat history
         st.session_state.messages.append({"role": "user", "content": prompt})
 
-        # docs = db.similarity_search(prompt)
         try:
             retriever=vectorstore.as_retriever()
             retrieval_chain=create_retrieval_chain(retriever,document_chain)
@@ -127,7 +140,7 @@ def prompted():
         except:
             response = chain.invoke(prompt)
             flag=1
-        if flag == 0:
+        if flag == 1:
         # Display assistant response in chat message container
             with st.chat_message("assistant"):
                 st.markdown(response)
@@ -135,13 +148,24 @@ def prompted():
             st.session_state.messages.append({"role": "assistant", "content": response})
         else:
             with st.chat_message("assistant"):
-                st.markdown(response["answer"])
+                st.markdown(response)
             # Add assistant response to chat history
-            st.session_state.messages.append({"role": "assistant", "content": response["answer"]})
+            st.session_state.messages.append({"role": "assistant", "content": response["context"]})
+            # doc_context = response["context"]
 
-  
-if (uploaded_files):
-    uploaded()
+def update():
+    st.write(model_selected)
+    st.title(model_selected)
+
+with st.sidebar:
+    uploaded_files = st.file_uploader("Please upload your files", accept_multiple_files=True, type=None)
+    st.button("Index files",on_click=uploaded)
+    model_selected = st.selectbox("Select Model ( qwen )",models)
+    store_selected = st.selectbox("select store", vstores)
+    if model_selected != st.session_state.model_selected:
+        st.session_state.model_selected = model_selected
+        
+
+
 if prompt:
     prompted()
-    uploaded()
